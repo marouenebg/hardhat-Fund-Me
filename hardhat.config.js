0486d2ff4856e0d0a268936fed1f34625dc49da2
marouenebg <marwenbg123@gmail.com>
2023-03-18 21:45:19 +0100

    adding multiple compilers version to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require ("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.8",
+module.exports = { 
+  solidity: {
+    compilers: [{version: "0.8.8"}, {version: "0.6.6"} ]
+  },
 };
